e002637491144878e501a47a504f7713de6c1f4d
xuyao <xuyao276@gmail.com>
2023-05-13 04:57:40 +0200

    titleComponent and StartButton

--- services/frontend/src/views/HomeView.vue
+++ services/frontend/src/views/HomeView.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="home">
+    <TitleComponent></TitleComponent>
     <img alt="Vue logo" src="../assets/logo.png">
+    <StartButtonComponent></StartButtonComponent>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    
+
   </div>
 </template>
 
 <script setup>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import TitleComponent from '@/components/TitleComponent.vue'
+import StartButtonComponent from '@/components/StartButtonComponent.vue'
+
 
 </script>
