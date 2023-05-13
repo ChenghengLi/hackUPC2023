df697c929269b377ae1279dfb58c00dd9c886a15
Chengheng Li <li471115381@gmail.com>
2023-05-13 10:12:19 +0200

    update requirements

--- services/backend/src/QuestionGenerator.py
+++ services/backend/src/QuestionGenerator.py
@@ -8,10 +8,12 @@ class QGenerator:
     
     def gerateMCQ(self):
         qg = main.QGen()
+        print(self.input_text)
         output = qg.predict_mcq(self.input_text)
         results = dict()
+        print(output["questions"])
         for i in range(len(output["questions"])):
-            results[i] = {"question": output["questions"][i]["'question_statement'"], 
+            results[i] = {"question": output["questions"][i]["question_statement"], 
                        "answer": output["questions"][i]["answer"],
                        "options": output["questions"][i]["options"]}
         return results
