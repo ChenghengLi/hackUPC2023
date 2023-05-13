8f8f3dbe28a0523948f04abb5bb8fa23578b0473
WW1027 <80759866+WW1027@users.noreply.github.com>
2023-05-13 04:57:43 +0200

    Upload Button

---
+++ services/frontend/src/components/UploadComponent.vue
@@ -0,0 +1,20 @@
+<template> 
+    <button type="button" class="btn" @click="handleButtonClick">
+    <img alt="upload the document here" src="../assets/upload.png" class="button-image">
+  </button>
+</template>
+
+<script setup>
+
+const handleButtonClick = () => {
+  // add your button click event handler code here
+  window.alert('Button clicked!');
+}
+</script>
+
+<style>
+.button-image{
+    width: 300px;
+    height: 250px;
+}
+</style>
